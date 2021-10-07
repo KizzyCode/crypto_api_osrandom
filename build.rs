77ce05fed028c56d9440abb8f15696faebf63b64
Keziah Biermann <development@kizzycode.de>
2021-10-07 21:41:30 +0200

    Update deprecated syntax

--- build.rs
+++ build.rs
@@ -20,7 +20,7 @@ fn linux_check_getrandom() -> Option<&'static str> {
 	
 	// Validate version
 	match (v[0], v[1]) {
-		(2...255, 25...255) => Some("USE_GETRANDOM"),
+		(2..=255, 25..=255) => Some("USE_GETRANDOM"),
 		_ => Some("USE_DEV_URANDOM")
 	}
 }
